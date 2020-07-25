3294badf0424fc4de9d807175249ccccb0321d20
iicg <iicgtesis2020@gmail.com>
2020-07-25 16:48:39 -0400

    bloquear y desbloquear usuario

--- src/components/AlumnoItem/index.js
+++ src/components/AlumnoItem/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Firebase } from '../../utils';
@@ -7,9 +7,16 @@ import './styles.css';
 
 export default function AlumnoItem(props) {
   const { alumno } = props;
+  const [loading, setLoading] = useState(false);
 
   const blockUser = useCallback(() => {
-    Firebase.admin.blockUser(alumno.uid);
+    setLoading(true);
+    if (alumno.bloqueado) Firebase.admin.unblockUser(alumno.uid);
+    else Firebase.admin.blockUser(alumno.uid);
+  }, [alumno]);
+
+  useEffect(() => {
+    setLoading(false);
   }, [alumno]);
 
   return (
@@ -20,11 +27,15 @@ export default function AlumnoItem(props) {
       <p className="alumno-item-descripcion">Rut: {alumno.rut}</p>
       <h5 className="alumno-item-descripcion">Tipo de plan: {alumno.tipoPlan}</h5>
       <div className="alumno-item-acciones">
-        <input
-          onClick={blockUser}
-          type="button"
-          value={alumno.bloqueado ? 'Desbloquear' : 'Bloquear'}
-        />
+        {loading ? (
+          <div>Cargando</div>
+        ) : (
+          <input
+            onClick={blockUser}
+            type="button"
+            value={alumno.bloqueado ? 'Desbloquear' : 'Bloquear'}
+          />
+        )}
         <input type="button" value="Editar" />
       </div>
     </div>
--- src/utils/firebase/admin.js
+++ src/utils/firebase/admin.js
@@ -1,7 +1,13 @@
+import { ReduxService } from '..';
+
 export function blockUser(uid) {
-  fetch(`https://us-central1-tesising2020.cloudfunctions.net/blockUser?uid=${uid}`);
+  fetch(`https://us-central1-tesising2020.cloudfunctions.net/blockUser?uid=${uid}`).then(() =>
+    ReduxService.dispatch(ReduxService.users.actions.modify({ uid, bloqueado: true })),
+  );
 }
 
 export function unblockUser(uid) {
-  fetch(`https://us-central1-tesising2020.cloudfunctions.net/unblockUser?uid=${uid}`);
+  fetch(`https://us-central1-tesising2020.cloudfunctions.net/unblockUser?uid=${uid}`).then(() =>
+    ReduxService.dispatch(ReduxService.users.actions.modify({ uid, bloqueado: false })),
+  );
 }

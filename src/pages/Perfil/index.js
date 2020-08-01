36aeac98077f233d699fcb2d2bfa3d329756f9ba
iicg <iicgtesis2020@gmail.com>
2020-08-01 14:57:59 -0500

    perfil usuario

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import useShallowEqualSelector from '../../shared/hooks/useShallowEqualSelector';
@@ -8,23 +9,21 @@ import { Header } from '../../components';
 
 export default function Perfil() {
   const session = useShallowEqualSelector(ReduxService.session.selectors.active);
-  const { nombre } = session;
-  const { apellido } = session;
-  const { fechaIngreso } = session;
-  const { tipoPlan } = session;
-  const { email } = session;
+  const { nombre, apellido, tipoPlan, email } = session;
 
   return (
     <div className="perfil-container">
       <Header />
+      <Link to="/home">
+        <button className="atras">volver</button>
+      </Link>
       <div className="perfil-page-content">
-        <div>
-          <h3 className="perfil-page-title">{nombre}</h3>
-          <h3 className="clase-page-title">{apellido}</h3>
+        <div className="perfil-nombre">
+          <h1 className="perfil-page-title">{nombre}</h1>
+          <h1 className="perfil-page-title">{apellido}</h1>
         </div>
-        <h4 className="clase-page-title">{email}</h4>
-        <h5 className="clase-page-title">{fechaIngreso}</h5>
-        <h5 className="clase-page-title">{tipoPlan}</h5>
+        <h4 className="perfil-page-title">email: {email}</h4>
+        <h5 className="perfil-page-title">tipo de plan: {tipoPlan}</h5>
       </div>
     </div>
   );

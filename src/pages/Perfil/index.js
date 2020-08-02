e673616fc4287b842bcd9d5b23866e0433b8e039
iicg <iicgtesis2020@gmail.com>
2020-08-02 13:20:46 -0500

    nueva flecha en perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -9,19 +9,22 @@ import { Header } from '../../components';
 
 export default function Perfil() {
   const session = useShallowEqualSelector(ReduxService.session.selectors.active);
-  const { nombre, apellido, tipoPlan, email } = session;
+  const { nombre, rut, apellido, tipoPlan, email } = session;
 
   return (
     <div className="perfil-container">
       <Header />
       <div className="perfil-page-content">
         <Link to="/home">
-          <button className="atras">volver</button>
+          <button type="button" className="main-back-button">
+            <span className="material-icons">arrow_back</span>
+          </button>
         </Link>
         <div className="perfil-nombre">
           <h1 className="perfil-page-title">{nombre}</h1>
           <h1 className="perfil-page-title">{apellido}</h1>
         </div>
+        <h3 className="perfil-page-title">{rut}</h3>
         <h4 className="perfil-page-title">email: {email}</h4>
         <h5 className="perfil-page-title">tipo de plan: {tipoPlan}</h5>
       </div>
